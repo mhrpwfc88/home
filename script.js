0834ba22dd4ba59c78a0de5bb83b32349781d55f
M H R P <mh17pwfc@gmail.com>
2022-06-16 18:42:14 +0700

    tambah fitur

---
+++ script.js
@@ -0,0 +1,9 @@
+window.setTimeout("waktu()", 1000);
+
+function waktu() {
+  var waktu = new Date();
+  setTimeout("waktu()", 1000);
+  document.getElementById("jam").innerHTML = waktu.getHours();
+  document.getElementById("menit").innerHTML = waktu.getMinutes();
+  document.getElementById("detik").innerHTML = waktu.getSeconds();
+}
