0dfaae4630d785359650a4168a6938b87c455265
mhrpwfc88 <mh17pwfc@gmail.com>
2022-08-28 15:18:46 -0700

    oke

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,25 @@ function waktu() {
   document.getElementById("menit").innerHTML = waktu.getMinutes();
   document.getElementById("detik").innerHTML = waktu.getSeconds();
 }
+
+
+onload = function () {
+        let id = document.getElementById("app");
+        let loading = document.createElement("div");
+
+        loading.textContent = "Loading";
+        loading.style.fontSize = "28px";
+        id.appendChild(loading);
+
+        let loaded = setInterval(() => {
+          loading.textContent = loading.textContent + ".";
+        }, 1000);
+
+        setTimeout(() => {
+          clearInterval(loaded);
+          loading.textContent = "WELCOME TO MY PAGE";
+        }, 4000);
+      };
+      setTimeout(() => {
+        window.location = "app.html";
+      }, 6000);
